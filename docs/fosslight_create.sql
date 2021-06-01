0e45ada146405b3d584322f408f219dc6b42004d
Soim Kim <soim.kim@lge.com>
2021-06-01 19:32:44 +0900

    Update sql statements

--- docs/fosslight_create.sql
+++ docs/fosslight_create.sql
@@ -1,50 +1,49 @@
--- --------------------------------------------------------
--- 호스트:                          
--- 서버 버전:                        PostgreSQL 9.3.24 on x86_64-unknown-linux-gnu, compiled by gcc (Ubuntu 4.8.4-2ubuntu1~14.04.4) 4.8.4, 64-bit
--- 서버 OS:                        
--- HeidiSQL 버전:                  10.2.0.5599
--- --------------------------------------------------------
+--
+-- PostgreSQL database dump
+--
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES  */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
--- bat 데이터베이스 구조 내보내기
-CREATE DATABASE IF NOT EXISTS `bat` DEFAULT CHARACTER SET utf8mb4;
-CREATE USER 'bin_analysis_script_user'@'%' IDENTIFIED BY 'script_123';
-CREATE USER 'bin_analysis_script_user'@'localhost' IDENTIFIED BY 'script_123';
-GRANT ALL PRIVILEGES ON bat.* TO 'bin_analysis_script_user'@'%';
-GRANT ALL PRIVILEGES ON bat.* TO 'bin_analysis_script_user'@'localhost';
-FLUSH PRIVILEGES;
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
 
-USE `bat`;
+SET default_tablespace = '';
 
--- 테이블 public.lgematching 구조 내보내기
-CREATE TABLE IF NOT EXISTS "lgematching" (
-	"filename" TEXT NULL DEFAULT NULL COMMENT E'',
-	"pathname" TEXT NULL DEFAULT NULL COMMENT E'',
-	"checksum" TEXT NULL DEFAULT NULL COMMENT E'',
-	"tlshchecksum" TEXT NULL DEFAULT NULL COMMENT E'',
-	"ossname" TEXT NULL DEFAULT NULL COMMENT E'',
-	"ossversion" TEXT NULL DEFAULT NULL COMMENT E'',
-	"license" TEXT NULL DEFAULT NULL COMMENT E'',
-	"parentname" TEXT NULL DEFAULT NULL COMMENT E'',
-	"platformname" CHARACTER VARYING(100) NULL DEFAULT NULL::character varying COMMENT E'',
-	"platformversion" CHARACTER VARYING(100) NULL DEFAULT NULL::character varying COMMENT E'',
-	"updatedate" TIMESTAMP WITHOUT TIME ZONE NULL DEFAULT NULL COMMENT E'',
-	"sourcepath" TEXT NULL DEFAULT NULL COMMENT E'',
-	KEY ("filename"),
-	KEY ("checksum")
+SET default_with_oids = false;
+
+
+--
+-- Name: lgematching; Type: TABLE; Schema: public; Owner: bat; Tablespace:
+--
+
+CREATE TABLE public.lgematching (
+    filename text,
+    pathname text,
+    checksum text,
+    tlshchecksum text,
+    ossname text,
+    ossversion text,
+    license text,
+    parentname text,
+    platformname character varying(100) DEFAULT NULL::character varying,
+    platformversion character varying(100) DEFAULT NULL::character varying,
+    updatedate timestamp without time zone,
+    sourcepath text,
+    UNIQUE (filename, checksum)
 );
 
--- 테이블 데이터 public.lgematching:506,495 rows 내보내기
-/*!40000 ALTER TABLE "lgematching" DISABLE KEYS */;
-INSERT INTO "lgematching" ("filename", "pathname", "checksum", "tlshchecksum", "ossname", "ossversion", "license", "parentname", "platformname", "platformversion", "updatedate", "sourcepath") VALUES
-	(E'askalono.exe', E'third_party/askalono/askalono.exe', E'3f5c6bbf06ddf53a46634bb21691ab0757f3b80c', E'T138267C12BB86A9EDC06AC470878646225B31B4CA0B25BFFF41C455743E6AAF45F3D39C', E'askalono', E'', E'Apache-2.0', E'[123]windows app project', E'windows', E'10', E'2021-02-19 17:21:52.430065', E'third_party/src/askalono'),
-	(E'askalono_macos', E'third_party/askalono/askalono_macos', E'b4856c381954151781c3a46739deccc2fb0e4f49', E'T117469D27F652686CE257C0701BDE57A26731F8350231AB6F37D4A6356E27DA09B8C383', E'askalono', E'', E'Apache-2.0', E'[1234]macOS app project', E'macOS', E'catalina', E'2021-01-08 09:13:16.718569', E'third_party/src/askalono');
 
-/*!40000 ALTER TABLE "lgematching" ENABLE KEYS */;
+--
+-- Data for Name: lgematching; Type: TABLE DATA; Schema: public; Owner: bat
+--
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+INSERT INTO public.lgematching (filename, pathname, checksum, tlshchecksum, ossname, ossversion, license, parentname, platformname, platformversion, updatedate, sourcepath) VALUES
+        ('askalono.exe', 'third_party/askalono/askalono.exe', '3f5c6bbf06ddf53a46634bb21691ab0757f3b80c', 'T138267C12BB86A9EDC06AC470878646225B31B4CA0B25BFFF41C455743E6AAF45F3D39C', 'askalono', '', 'Apache-2.0', '[123]windows app project', 'windows', '10', '2021-02-19 17:21:52.430065', 'third_party/src/askalono'),
+        ('askalono_macos', 'third_party/askalono/askalono_macos', 'b4856c381954151781c3a46739deccc2fb0e4f49', 'T117469D27F652686CE257C0701BDE57A26731F8350231AB6F37D4A6356E27DA09B8C383', 'askalono', '', 'Apache-2.0', '[1234]macOS app project', 'macOS', 'catalina', '2021-01-08 09:13:16.718569', 'third_party/src/askalono');
+
+
+GRANT ALL PRIVILEGES ON public.lgematching TO bin_analysis_script_user;
+
