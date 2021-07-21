63dd89cb09f97efc7fa07e048b730a0d8f339fd5
Soim Kim <soim.kim@lge.com>
2021-07-21 17:13:56 +0900

    Modify the license to comply with reuse

--- hooks/hook-fosslight_binary.py
+++ hooks/hook-fosslight_binary.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2021 LG Electronics
+
 from PyInstaller.utils.hooks import collect_all
 
 datas, binaries, hiddenimports = collect_all('fosslight_binary')
