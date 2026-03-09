fd8ca0360eecb70161ef73b72c108bb9b28887ec
Jaekwon Bang <jaekwon.bang@lge.com>
2026-03-09 16:05:50 +0900

    Fix tox error

--- hooks/hook-fosslight_binary.py
+++ hooks/hook-fosslight_binary.py
@@ -6,3 +6,9 @@
 from PyInstaller.utils.hooks import collect_all
 
 datas, binaries, hiddenimports = collect_all('fosslight_binary')
+
+# Collect binaryornot package including data files
+datas_binaryornot, binaries_binaryornot, hiddenimports_binaryornot = collect_all('binaryornot')
+datas += datas_binaryornot
+binaries += binaries_binaryornot
+hiddenimports += hiddenimports_binaryornot
